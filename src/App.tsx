ccf929685c2945552a54ffe7f521459b4b15c3c9
willian-souza-apte <willian.souza@apte.com.br>
2022-11-17 18:28:58 -0300

    Adicionado theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css';
 import AppBar from "./components/AppBar";
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material';
+
 import { AppRouter } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppBar />
-      <AppRouter />
-    </BrowserRouter>
-    
+    <ThemeProvider theme={LightTheme}>  
+      <BrowserRouter>
+        <AppBar />
+        <AppRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
